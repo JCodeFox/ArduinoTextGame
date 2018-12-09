35f118dc65ed61a84a9898ab436c5bfe06787445
JCodeFox <36343336+JCodeFox@users.noreply.github.com>
2018-12-08 19:05:48 -0500

    Add function to get text from a file.

--- ArduinoTextGame/ArduinoTextGame.ino
+++ ArduinoTextGame/ArduinoTextGame.ino
@@ -5,6 +5,7 @@
 #include <SD.h>
 #include <ArduinoJson.h>
 
+const String extension=".txt";
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
@@ -13,6 +14,14 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 int cents=10000;
 
 void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("initialization done.");
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   lcd.setCursor(0, 0);
@@ -23,4 +32,24 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+String getTextFromFile(String fileName){
+    //All text stored in the file is put in this variable.
+    String result="";
+    File myFile=SD.open(fileName+extension);
+    if (myFile) {
+    Serial.println("test.txt:");
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      result=result+myFile.read();
+    }
+      // close the file:
+      myFile.close();
+    } else {
+      // if the file didn't open, print an error:
+      Serial.println("error opening "+fileName+extension);
+    }
+    return result;
 }
